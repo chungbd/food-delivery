fe079673958e046407266cba7d26d87cd39f99e3
chungbd <chungvotinh@gmail.com>
2021-02-25 21:52:37 +0700

    update note business and storage

---
+++ module/note/notebusiness/delete_note.go
@@ -0,0 +1,43 @@
+package notebusiness
+
+import (
+	"errors"
+	"food-delivery/module/note/notemodel"
+)
+
+type DeleteNoteStore interface {
+	FindDataWithCondition(condition map[string]interface{}) (*notemodel.Note, error)
+	Delete(id int) error
+}
+
+type deleteNoteBiz struct {
+	store DeleteNoteStore
+}
+
+func NewDeleteNoteBiz(store DeleteNoteStore) *deleteNoteBiz {
+	return &deleteNoteBiz{store: store}
+}
+
+func (biz *deleteNoteBiz) DeleteNote(noteId int) error {
+	// Find note by id
+	// if old data has status is 0
+	// => error: note has been deleted
+	// => error: note have been deleted
+	// else
+	// delete note
+	note, err := biz.store.FindDataWithCondition(map[string]interface{}{"id": noteId})
+
+	if err != nil {
+		return err
+	}
+
+	if note.Status == 0 {
+		return errors.New("note has been deleted before")
+	}
+
+	if err := biz.store.Delete(note.ID); err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ module/note/notestorage/store.go
@@ -0,0 +1,12 @@
+package notestorage
+
+import "gorm.io/gorm"
+
+type store struct {
+	db *gorm.DB
+}
+
+// NewSQLStore - Creating store pointer
+func NewSQLStore(db *gorm.DB) *store {
+	return &store{db: db}
+}
