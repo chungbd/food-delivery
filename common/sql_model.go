0d9db72a0535c0f25d8c0b8afc59372ade6d1639
chungbd <chungvotinh@gmail.com>
2021-03-02 18:42:36 +0700

    fix null value for timestamp

--- common/sql_model.go
+++ common/sql_model.go
@@ -4,7 +4,7 @@ import "time"
 
 type SQLModel struct {
 	ID        int        `json:"id" gorm:"id"`
-	Status    int        `json:"status" gorm:"status"`
+	Status    int        `json:"status"`
 	CreatedAt *time.Time `json:"created_at" `
 	UpdatedAt *time.Time `json:"updated_at" gorm:"updated_at"`
 }
